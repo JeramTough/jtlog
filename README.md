12a65190193dea5022a94d59e95cd0ed714ef17f
JeramTough <1171867004@qq.com>
2018-08-25 19:34:25 +0800

    Signed-off-by: JeramTough <1171867004@qq.com>

--- README.md
+++ README.md
@@ -6,13 +6,64 @@
 &emsp;&emsp;A logging library for javaweb project or android project. 
 
 ---
+### 环境要求
+JDK1.8及以上
+
+---
+
 
 ### 添加依赖
 jtlog/build/libs/jtlog-*.jar
 
 ---
 ### 简单使用
-1. 使用全局日志工具类 
+1. 使用JtLogger接口
+
+```
+JtLogger jtLogger = JtLoggerManager.getJtLogger(TestMain.class); 
+jtLogger.arrive();
+jtLogger.info("11111");
+jtLogger.warn("22222");
+jtLogger.debug("tag", "3333");
+jtLogger.debug("44444");
+jtLogger.error("tag", "55555");
+jtLogger.verbose("tag", "66666");
+jtLogger.p("77777");
+
+```
+*效果：*
+
+```
+I:{time}=19:30:31.390 , {thread}=main , {context}=TestMain
+11111
+
+W:{time}=19:30:31.409 , {thread}=main , {context}=TestMain
+22222
+
+D:{time}=19:30:31.410 , {thread}=main , {context}=TestMain , {tag}=tag , {location}=TestMain.test().31 , {caller}=(TestMain.java:31)
+3333
+
+D:{time}=19:30:31.410 , {thread}=main , {context}=TestMain , {location}=TestMain.test().32 , {caller}=(TestMain.java:32)
+44444
+
+E:{time}=19:30:31.411 , {thread}=main , {context}=TestMain , {tag}=tag
+55555
+
+V:{time}=19:30:31.412 , {thread}=main , {context}=TestMain , {tag}=tag
+66666
+
+Arrive:{time}=19:30:31.412 , {thread}=main , {context}=TestMain , {location}=TestMain.test().35 , {caller}=(TestMain.java:35)
+77777
+```
+
+
+2. 使用全局日志工具类 
+
+```
+
+```
+
+3. 实现WithJtLogger接口 
 
 ```
 
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -6,7 +6,7 @@ import com.jeramtough.jtlog.jtlogger.JtLoggerManager;
 import com.jeramtough.jtlog.level.LogLevel;
 import com.jeramtough.jtlog.with.WithJtLogger;
 
-@JtLoggerConfig(isUsedJtloggerApi = false, isEnabled = true,
+@JtLoggerConfig(isUsedJtloggerApi = true, isEnabled = true,
         minVisibleLevel = LogLevel.VERBOSE)
 public class TestMain implements WithJtLogger {
     public static void main(String[] args) {
@@ -26,17 +26,18 @@ public class TestMain implements WithJtLogger {
         TagLogFilter tagLogFilter = new TagLogFilter("aaa");
         jtLogger.getLogContext().getLogConfig().addLogFilter(tagLogFilter);
 
-        jtLogger.info("infoinfo");
-        jtLogger.warn("warnwarn");
-        jtLogger.debug("adfsa", "debugdebug");
-        jtLogger.error("aaa", "error error");
-        jtLogger.verbose("bb", "verbose,verbose");
         jtLogger.arrive();
-        jtLogger.p(1 + 6);
+        jtLogger.info("11111");
+        jtLogger.warn("22222");
+        jtLogger.debug("tag", "3333");
+        jtLogger.debug("44444");
+        jtLogger.error("tag", "55555");
+        jtLogger.verbose("tag", "66666");
+        jtLogger.p("77777");
     }
 
     private void test1() {
-        L.debug("L.infoinfoinfo");
+        L.debug("this is a debug information");
     }
     private void test2() {
         getJtLogger().debug("with.www");
