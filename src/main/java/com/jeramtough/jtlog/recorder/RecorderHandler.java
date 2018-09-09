897fb94817252553d8a82389e8066d8fe51750e8
JeramTough <1171867004@qq.com>
2018-09-09 11:55:58 +0800

    Signed-off-by: JeramTough <1171867004@qq.com>

--- src/main/java/com/jeramtough/jtlog/recorder/RecorderHandler.java
+++
@@ -1,9 +0,0 @@
-package com.jeramtough.jtlog.recorder;
-
-/**
- * Created on 2018-09-07 11:27
- * by @author JeramTough
- */
-public interface RecorderHandler {
-    Recorder[] handleRecorders();
-}
