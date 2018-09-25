1a75154fda18e1ddbc5c5017286ac7b286e7ed32
JeramTough <1171867004@qq.com>
2018-09-25 11:14:02 +0800

    修复SimpleLogCofigDefaultValues的bug

--- src/main/java/com/jeramtough/jtlog/log/config/SimpleLogConfigDefaultValues.java
+++ src/main/java/com/jeramtough/jtlog/log/config/SimpleLogConfigDefaultValues.java
@@ -6,29 +6,29 @@ import com.jeramtough.jtlog.level.LogLevel;
  * Created on 2018-09-25 02:00
  * by @author JeramTough
  */
-public  class SimpleLogConfigDefaultValues implements LogConfigDefaultValues {
+public class SimpleLogConfigDefaultValues implements LogConfigDefaultValues {
     @Override
     public int loadMaxLengthOfRow() {
-        return 0;
+        return JtLogConfigDefaultValues.MAX_LENGTH_OF_ROM;
     }
 
     @Override
     public boolean loadIsEnabled() {
-        return false;
+        return JtLogConfigDefaultValues.IS_ENABLED;
     }
 
     @Override
     public boolean loadIsUsedJtloggerApi() {
-        return false;
+        return JtLogConfigDefaultValues.IS_USED_JTLOGGER_API;
     }
 
     @Override
     public LogLevel loadMinVisibleLevel() {
-        return null;
+        return JtLogConfigDefaultValues.MIN_VISIBLE_LEVEL;
     }
 
     @Override
     public int loadCallerPlus() {
-        return 0;
+        return JtLogConfigDefaultValues.CALLER_PLUS;
     }
 }
