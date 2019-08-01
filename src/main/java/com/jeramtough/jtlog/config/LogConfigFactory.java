1b6b5011c89a2b849a1c2fd2170a370fd19840df
JeramTough <1171867004@qq.com>
2019-08-01 18:28:42 +0800

    新增生产环境使用外部json配置文件的功能

---
+++ src/main/java/com/jeramtough/jtlog/config/LogConfigFactory.java
@@ -0,0 +1,13 @@
+package com.jeramtough.jtlog.config;
+
+/**
+ * Created on 2019-08-01 14:39
+ * by @author JeramTough
+ */
+public interface LogConfigFactory {
+
+    LogConfig getDefaultValueLogConfig(String contextName);
+
+    LogConfig getLogConfigByAnnotation(String contextName,
+                                       Class classWithLogConfigerAnnotation);
+}
